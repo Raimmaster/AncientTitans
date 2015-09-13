d3f38db1442e6b27c0b3202a24e60b176cdf8212
Juan Venegas <jrvboesch@gmail.com>
2015-09-13 12:04:23 -0600

    lots of things

--- xuan.gd
+++ xuan.gd
@@ -5,6 +5,11 @@ extends KinematicBody2D
 # var a=2
 # var b="textvar"
 var anim = ""
+var velocidad = Vector2()
+
+const GRAVITY = 500
+const MOVE_SPEED = 50
+const JUMP_SPEED = 300
 
 func _ready():
 	# Initialization here
@@ -18,20 +23,37 @@ func _fixed_process(delta):
 	var jump = Input.is_action_pressed("jump")
 	var attack = Input.is_action_pressed("attack")
 	var spin = Input.is_action_pressed("spin_attack")
+	var special = Input.is_action_pressed("special_attack")
 	
+	velocidad.y += delta * GRAVITY
 	if move_right:
 		new_anim = "right"
+		velocidad.x = MOVE_SPEED
 	elif move_left:
 		new_anim = "left"
+		velocidad.x = - MOVE_SPEED
 	elif jump:
 		new_anim = "jump"
+		velocidad.y = -JUMP_SPEED 
 	elif attack:
 		new_anim = "basic"
 	elif spin:
 		new_anim = "spin"
+	elif special:
+		new_anim = "special"
+	else:
+		velocidad.x = 0
+		
+	var motion = velocidad * delta
+	motion = move(motion)
 	
 	if(anim != new_anim):
 		anim = new_anim
+		self.get_node("AnimationPlayer").play(anim)
 		
-	self.get_node("AnimationPlayer").play(anim)
-	new_anim = ""
+	if(is_colliding()):
+		var norm = get_collision_normal()
+		velocidad.y = 0
+		motion = norm.slide(motion)
+		velocidad = norm.slide(velocidad)
+		move(motion)
